ffff90fbdb336269cbfb92adcc9f289499b03a2a
Edil <edil.asanbaev.kk@gmail.com>
2022-11-30 22:52:31 +0600

    react-pizza

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'; 
+import { useState } from 'react';
 
 function App() {
+ 
+  const [number, setNumber] = useState(0)
+
+  function add (){
+    setNumber(number + 1)
+  };
+
+  function remove (){
+    if(number == 0){
+      alert("Больше нельзя минусовать")
+    }else{
+      setNumber(number - 1)
+    }
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <h1>Ваша цифра: {number}</h1>
+      <button onClick={add}>Добавить</button>
+      <button onClick={remove}>Удалить</button>
+  
     </div>
+
+    
   );
 }
 
