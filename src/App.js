6e1909cbaded18362477fc5519684b44eeccdbe2
Edil <edil.asanbaev.kk@gmail.com>
2022-12-07 19:19:54 +0600

    m

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import './App.css';
-import React from 'react'; 
+import React from 'react';
 import { useState } from 'react';
 
 function App() {
- 
-  const [number, setNumber] = useState(0)
+  const [number, setNumber] = useState(0);
 
-  function add (){
-    setNumber(number + 1)
-  };
+  function add() {
+    setNumber(number + 2);
+  }
 
-  function remove (){
-    if(number == 0){
-      alert("Больше нельзя минусовать")
-    }else{
-      setNumber(number - 1)
+  function remove() {
+    if (number == 0) {
+      alert('Больше нельзя минусовать');
+    } else {
+      setNumber(number - 1);
     }
-  };
- 
+  }
+
   return (
-    
     <div>
       <h1>Ваша цифра: {number}</h1>
       <button onClick={add}>Добавить</button>
       <button onClick={remove}>Удалить</button>
-  
     </div>
-
-    
   );
 }
 
